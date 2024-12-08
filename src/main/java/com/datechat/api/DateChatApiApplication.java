fb34f9bbcba112eee1d80b5c1334b40d55e85ea6
perlowicz <bartek.perlinski1234@gmail.com>
2024-12-08 18:26:06 +0100

    Initial commit

---
+++ src/main/java/com/datechat/api/DateChatApiApplication.java
@@ -0,0 +1,13 @@
+package com.datechat.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateChatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DateChatApiApplication.class, args);
+    }
+
+}
